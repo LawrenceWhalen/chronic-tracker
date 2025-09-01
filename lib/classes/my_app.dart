392ca67f2a5f23246b5da39270a68ea2f34df90b
Lawrence Whalen <ldwhalen@gmail.com>
2025-08-31 18:05:57 -0600

    Refactor my_app to remove theme data

--- lib/classes/my_app.dart
+++ lib/classes/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:chronic_tracker/navigation/app_routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -6,10 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chronic Tracker',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      initialRoute: '/',
+      routes: appRoutes
     );
   }
 }
