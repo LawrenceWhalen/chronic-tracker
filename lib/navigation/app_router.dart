43c3cf5742d396dc7eda00950568bd10c3155a1b
Lawrence Whalen <ldwhalen@gmail.com>
2025-09-01 12:51:01 -0600

    create global key for navigation

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,11 +1,17 @@
 import 'package:chronic_tracker/screens/test_screen.dart';
 import 'package:chronic_tracker/screens/home_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-final _appRouter = GoRouter(
+final _trunkRouteKey = GlobalKey<NavigatorState>();
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  navigatorKey: _trunkRouteKey,
   routes: [
     GoRoute(
       path: '/',
+      //GoRouterState state
       builder: (context, state) => HomeScreen(),
     ),
     GoRoute(
