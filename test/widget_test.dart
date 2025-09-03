09222217755b0713e98b78b5da344fe53949a62f
Lawrence Whalen <ldwhalen@gmail.com>
2025-09-03 13:14:25 -0600

    Write test for navigatin button widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,43 @@ import 'package:chronic_tracker/classes/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
+import 'package:chronic_tracker/widgets/button_widgets.dart';
 
 void main() {
   testWidgets('App Boot Test', (WidgetTester tester) async {
 
     //load app widget
     await tester.pumpWidget(const MyApp());
+
+  });
+
+  testWidgets('Navigation Button Widget Build Test', (WidgetTester tester) async {
+
+    //create a navigation button for a different page
+    await tester.pumpWidget(customNavigationBarButton(
+       buttonBuildMap: {
+         'icon': Icons.favorite,
+         'label': 'Favorite',
+         'isCurrentLocation': false
+       }
+    ));
+
+    //expect to find the button without any text
+    expect(find.byIcon(Icons.favorite), findsOneWidget);
+    expect(find.text('Favorite'), findsNothing);
+
+    //create a navigation button for the current page
+    await tester.pumpWidget(customNavigationBarButton(
+        buttonBuildMap: {
+          'icon': Icons.abc,
+          'label': 'Alphabet',
+          'isCurrentLocation': false
+        }
+    ));
+
+    //expect to find the button with text
+    expect(find.byIcon(Icons.abc), findsOneWidget);
+    expect(find.text('Alphabet'), findsOneWidget);
 
   });
 
@@ -36,6 +67,7 @@ void main() {
     expect(find.byKey(const ValueKey('test screen')), findsOneWidget);
     final testButton = find.byKey(const ValueKey('secondNavigationButton'));
     await tester.tap(testButton);
+    tester.pump();
 
     //Check we are on the test screen
     expect(GoRouterState.of(context).uri.toString(), equals(routeList[1]));
