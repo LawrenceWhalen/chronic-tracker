e83b69ca1cf21a81bffe47a9cea3f6d3aa573abb
Lawrence Whalen <ldwhalen@gmail.com>
2025-09-03 12:32:13 -0600

    Refactor test to remove hardcoded route names

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:chronic_tracker/classes/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:chronic_tracker/main.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   testWidgets('App Boot Test', (WidgetTester tester) async {
@@ -21,10 +21,24 @@ void main() {
   testWidgets('Navigation bar Test', (WidgetTester tester) async {
 
     //load app widget
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: Material(child: Container())));
+    final BuildContext context = tester.element(find.byType(Container));
+
+    var routeList = GoRouter.of(context).configuration.routes;
 
     //search for navigation bar by key
     expect(find.byKey(const ValueKey('navigationBar')), findsOneWidget);
+
+    //Check we are on the first screen
+    expect(GoRouterState.of(context).uri.toString(), equals(routeList[0]));
+
+    //Find and press the test screen button
+    expect(find.byKey(const ValueKey('test screen')), findsOneWidget);
+    final testButton = find.byKey(const ValueKey('secondNavigationButton'));
+    await tester.tap(testButton);
+
+    //Check we are on the test screen
+    expect(GoRouterState.of(context).uri.toString(), equals(routeList[1]));
 
   });
 }
