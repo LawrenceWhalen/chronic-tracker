e67e95a6640b17ddb5558d3448fad6ae31c32f8f
Lawrence Whalen <ldwhalen@gmail.com>
2025-09-02 19:53:12 -0600

    Refactor router with clear path names

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,16 +1,12 @@
 import 'package:chronic_tracker/screens/test_screen.dart';
 import 'package:chronic_tracker/screens/home_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-final _trunkRouteKey = GlobalKey<NavigatorState>();
-
 final appRouter = GoRouter(
-  initialLocation: '/',
-  navigatorKey: _trunkRouteKey,
+  initialLocation: '/homeScreen',
   routes: [
     GoRoute(
-      path: '/',
+      path: '/homeScreen',
       //GoRouterState state
       builder: (context, state) => HomeScreen(),
     ),
