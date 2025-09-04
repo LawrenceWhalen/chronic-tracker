7cc374d1c0fa77ee86980b9f6b3dd18191d9dfe1
Lawrence Whalen <ldwhalen@gmail.com>
2025-09-04 15:53:07 -0600

    create custom map class for nav button build

---
+++ lib/resources/buildMaps.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavButtonBuildMap {
+  Icon icon;
+  String label;
+  String path;
+  bool isCurrentLocation;
+
+  NavButtonBuildMap(this.icon, this.label, this.path, this.isCurrentLocation);
+}
